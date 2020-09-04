ddb3ebdff528fc274062ecea4dcbfde82a831fbf
selvert <sebastian.smolinski.stud@pw.edu.pl>
2020-09-04 15:45:47 +0200

    Added server

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"log"
+
+	"github.com/gin-gonic/autotls"
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -9,6 +13,29 @@ type task struct {
 	gorm.Model
 	Completed   bool
 	Description string
+}
+
+const (
+	port = ":8080"
+)
+
+func initial(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Hi, friend",
+	})
+	return
+}
+
+func invalid(c *gin.Context) {
+	c.JSON(404, gin.H{
+		"message": "Unable to proceed",
+	})
+	return
+}
+
+func routing(router *gin.Engine) {
+	router.GET("/index", initial)
+	router.NoRoute(invalid)
 }
 
 func main() {
@@ -20,6 +47,7 @@ func main() {
 		panic("failed to connect database")
 	}
 	// Migrate the schema
+	db.Migrator().DropTable(&task{})
 	db.AutoMigrate(&task{})
 
 	// Create
@@ -33,10 +61,17 @@ func main() {
 	// Update - update product's price to 200
 	db.Model(&product).Update("Completed", false)
 	// Update - update multiple fields
-	db.Model(&product).Updates(task{Completed: false, Description: "F42"}) // non-zero fields
-	db.Model(&product).Updates(map[string]interface{}{"Completed": false, "Description": "F42"})
+	db.Model(&product).Updates(task{Completed: false, Description: "R2D2"}) // non-zero fields
+	db.Model(&product).Updates(map[string]interface{}{"Completed": false, "Description": "R2D2"})
 
-	// Delete - delete product
-	//db.Delete(&product, 1)
+	//Delete - delete product
+	db.Delete(&product, 1)
+
+	router := gin.Default()
+	routing(router)
+
+	router.Run(port)
+
+	log.Fatal(autotls.Run(router))
 
 }
