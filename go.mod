43be24f3f00fb7fc6ae2c6d2b4492e75ae6eca93
selvert <sebastian.smolinski.stud@pw.edu.pl>
2020-09-05 17:02:59 +0200

    Showing tasks

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module main
 go 1.15
 
 require (
-	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/gin-gonic/autotls v0.0.3
+	github.com/gin-gonic/gin v1.6.3
 	gorm.io/driver/postgres v1.0.0
 	gorm.io/gorm v1.20.0
 )
--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"time"
 
 	"github.com/gin-gonic/autotls"
 	"github.com/gin-gonic/gin"
@@ -10,9 +12,12 @@ import (
 )
 
 type task struct {
-	gorm.Model
-	Completed   bool
-	Description string
+	ID          uint      `json:"id" gorm:"primary_key"`
+	Title       string    `json:"title"`
+	CreatedAt   time.Time `json:"createdat"`
+	UpdatedAt   time.Time `json:"updatedat"`
+	Completed   bool      `json:"completed"`
+	Description string    `json:"description"`
 }
 
 const (
@@ -33,40 +38,64 @@ func invalid(c *gin.Context) {
 	return
 }
 
+func findtasks(c *gin.Context) {
+	var tasks []task
+	db.Find(&tasks)
+
+	c.JSON(http.StatusOK, gin.H{"data": tasks})
+}
+
 func routing(router *gin.Engine) {
 	router.GET("/index", initial)
+	router.GET("/tasks", findtasks)
 	router.NoRoute(invalid)
+}
+
+var db *gorm.DB
+
+func connect() {
+	dsn := "user=postgres password=s197328645S! dbname=todo port=5432 sslmode=disable TimeZone=Europe/Warsaw"
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("Failed to connect to database!")
+	}
+
+	database.AutoMigrate(&task{})
+
+	db = database
 }
 
 func main() {
 
-	dsn := "user=postgres password=s197328645S! dbname=todo port=5432 sslmode=disable TimeZone=Europe/Warsaw"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	// dsn := "user=postgres password=s197328645S! dbname=todo port=5432 sslmode=disable TimeZone=Europe/Warsaw"
+	// db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
-	if err != nil {
-		panic("failed to connect database")
-	}
+	// if err != nil {
+	// 	panic("failed to connect database")
+	// }
 	// Migrate the schema
-	db.Migrator().DropTable(&task{})
-	db.AutoMigrate(&task{})
+	//db.Migrator().DropTable(&task{})
+	//db.AutoMigrate(&task{})
 
 	// Create
-	db.Create(&task{Description: "D42", Completed: true})
+	//db.Create(&task{Description: "Wash the dishes", Completed: true})
 
 	// Read
-	var product task
-	db.First(&product, 1)                        // find product with integer primary key
-	db.First(&product, "Description = ?", "D42") // find product with code D42
+	// var product task
+	// db.First(&product, 1)                        // find product with integer primary key
+	// db.First(&product, "Description = ?", "D42") // find product with code D42
 
-	// Update - update product's price to 200
-	db.Model(&product).Update("Completed", false)
-	// Update - update multiple fields
-	db.Model(&product).Updates(task{Completed: false, Description: "R2D2"}) // non-zero fields
-	db.Model(&product).Updates(map[string]interface{}{"Completed": false, "Description": "R2D2"})
+	// // Update - update product's price to 200
+	// db.Model(&product).Update("Completed", false)
+	// // Update - update multiple fields
+	// db.Model(&product).Updates(task{Completed: false, Description: "R2D2"}) // non-zero fields
+	// db.Model(&product).Updates(map[string]interface{}{"Completed": false, "Description": "R2D2"})
 
-	//Delete - delete product
-	db.Delete(&product, 1)
-
+	// //Delete - delete product
+	// db.Delete(&product, 1)
+	connect()
+	db.Create(&task{Description: "Wash the dishes", Completed: true})
 	router := gin.Default()
 	routing(router)
 
